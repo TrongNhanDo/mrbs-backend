54e1af5b1764785c2cc9e1ceaf2da50da8acb652
dtnhan <dotrongnhan15102000@gmail.com>
2023-12-07 13:48:58 +0700

    fix: add eslint and config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+   env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      node: true,
+   },
+   extends: 'eslint:recommended',
+   overrides: [],
+   parserOptions: {
+      ecmaVersion: 'latest',
+   },
+   rules: {
+      'no-unused-vars': [
+         'warn',
+         { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+      ],
+   },
+};
