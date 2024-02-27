07db280adb112e6809a70b673721851fda452a9e
dtnhan <dotrongnhan15102000@gmail.com>
2024-02-27 20:39:19 +0700

    fix: update addUserRules

--- src/controllers/user/validations.ts
+++ src/controllers/user/validations.ts
@@ -2,8 +2,39 @@ import { body } from 'express-validator';
 
 export const addUserRules = [
   body('name')
-    .isString()
-    .withMessage('The name field must be string.')
     .notEmpty()
     .withMessage('The name field is a required field.')
+    .if((value) => value !== undefined)
+    .isString()
+    .withMessage('The name field must be string.'),
+  body('level')
+    .notEmpty()
+    .withMessage('The level field is a required field.')
+    .if((value) => value !== undefined)
+    .isInt()
+    .withMessage('The level field must be number.'),
+  body('display_name')
+    .notEmpty()
+    .withMessage('The display_name field is a required field.')
+    .if((value) => value !== undefined)
+    .isString()
+    .withMessage('The display_name field must be string.'),
+  body('password_hash')
+    .notEmpty()
+    .withMessage('The password_hash field is a required field.')
+    .if((value) => value !== undefined)
+    .isString()
+    .withMessage('The password_hash field must be string.'),
+  body('email')
+    .notEmpty()
+    .withMessage('The email field is a required field.')
+    .if((value) => value !== undefined)
+    .isEmail()
+    .withMessage('The email field must be string.'),
+  body('last_login')
+    .notEmpty()
+    .withMessage('The last_login field is a required field.')
+    .if((value) => value !== undefined)
+    .isInt()
+    .withMessage('The last_login field must be string.')
 ];
